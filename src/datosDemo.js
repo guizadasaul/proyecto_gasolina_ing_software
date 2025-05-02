57594503b056082264a55d5e612c1f554b089242
GatoCoder282 <diegomvaldez19@gmail.com>
2025-05-02 17:50:34 -0400

    refactor:creacion de datos demo

---
+++ src/datosDemo.js
@@ -0,0 +1,24 @@
+export const datosDemo = [
+    { 
+      nombre: 'Gasolinera 1', 
+      estaActiva: true, 
+      direccion: 'av. santa cruz', 
+      coords: [-17.374706203158816, -66.15689669717388],
+      stock: { magna: 10, premium: 5, diesel: 0 } 
+    },
+    { 
+      nombre: 'Gasolinera 2', 
+      estaActiva: false, 
+      direccion: 'av. libertador',
+      coords: [-17.375544747649325, -66.16151967753312],
+      stock: { magna: 0, premium: 0, diesel: 20 } 
+    },
+    { 
+      nombre: 'Gasolinera 3', 
+      estaActiva: true, 
+      direccion: 'av. juan de la rosa',
+      coords: [-17.379139442834234, -66.16575999447656],
+      stock: { magna: 7, premium: 3, diesel: 1 } 
+    },
+  ];
+  
