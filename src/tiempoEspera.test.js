596e1a56446b4114225deae4c3b8eb239fd2b4ec
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-05-04 22:24:43 -0400

    test: añadimos nuevos test para el tiempo de espera

--- src/tiempoEspera.test.js
+++ src/tiempoEspera.test.js
@@ -5,5 +5,9 @@ describe('calcularTiempoEspera', () => {
     expect(calcularTiempoEspera(10, 2)).toBe(5);
   });
 
-  
+  test('Debe devolver 0 si no hay autos en la fila', () => {
+    expect(calcularTiempoEspera(0, 5)).toBe(0);
+  });
+
+
 });
