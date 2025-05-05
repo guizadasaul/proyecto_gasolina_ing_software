3d226303ea9484d8adcc82a9c650e8b9c6b1e30b
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-05-04 23:37:36 -0400

    feat: añadimos la funcionalidad de tiempoEspera para en el presenter y el index

--- src/datosDemo.js
+++ src/datosDemo.js
@@ -4,21 +4,27 @@ export const datosDemo = [
       estaActiva: true, 
       direccion: 'av. santa cruz', 
       coords: [-17.374706203158816, -66.15689669717388],
-      stock: { magna: 10, premium: 5, diesel: 0 } 
+      stock: { magna: 10, premium: 5, diesel: 0 } ,
+      capacidad: 2,
+      fila: 10
     },
     { 
       nombre: 'Gasolinera 2', 
       estaActiva: false, 
       direccion: 'av. libertador',
       coords: [-17.375544747649325, -66.16151967753312],
-      stock: { magna: 0, premium: 0, diesel: 20 } 
+      stock: { magna: 0, premium: 0, diesel: 20 },
+      capacidad: 3,
+      fila: 0 
     },
     { 
       nombre: 'Gasolinera 3', 
       estaActiva: true, 
       direccion: 'av. juan de la rosa',
       coords: [-17.379139442834234, -66.16575999447656],
-      stock: { magna: 7, premium: 3, diesel: 1 } 
+      stock: { magna: 7, premium: 3, diesel: 1 },
+      capacidad: 5,
+      fila: 20,
     },
   ];
   
